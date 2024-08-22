336932c0bd544b95397a8e750664d0b8987ee3c1
fps_netodf <fps_netodf@hotmail.com>
2024-08-22 16:45:08 -0300

    navegando home /sobre /contatos

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.set('view engine', 'html')
 app.use(express.static(path.join(__dirname, "static")))
 
 const PORT = '8000'
+app.get('/sobre', (req, res) => {
+  
+res.render('home/sobre.html')
+})
+
+app.get('/contatos', (req, res) => { 
+  const lista = ["Helder", "Hibsen", "Neto", "Lourenço"] 
+  res.render('home/contatos.html', {"lista": lista})
+  })
 
 app.get('/', (req, res) => {
     const lista = ["Francisco", "Alanna", "Wendy", "Vitoria", "Bruno"]
